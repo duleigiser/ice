1d81769fafa29831970a9acdc7f912e446197678
dulei <564779666@qq.com>
2018-11-15 19:51:36 +0800

    建立access_token模型

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000
 const r = path => resolve(__dirname, path)
 const config = require('../nuxt.config.js')
 config.dev = !(process.env === 'production')
-const MIDDLEWARE = ['router']
+const MIDDLEWARE = ['router', 'database']
 
 class Server {
   constructor() {
