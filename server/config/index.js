6518b8b4c21655ce51b78a9a22779eab0b17b10b
dulei <564779666@qq.com>
2018-11-20 16:33:23 +0800

    修改config

--- server/config/index.js
+++ server/config/index.js
@@ -2,7 +2,7 @@ export default {
   wechat: {
     token: 'wechat',
     appID: 'wx568823a4a4a0a412',
-    appsecret: '492cd553bde135a985f1f46ab0682aba'
+    appSecret: '492cd553bde135a985f1f46ab0682aba'
   },
   db: 'mongodb://172.16.164.44/ice'
 }
